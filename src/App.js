53dd1ede7229da64c4d5d1b4e37751bbadde0a0c
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-08-06 21:54:22 +0530

    Added Header to Blog App layout. Package and  lock files were changed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [username, setUserName] = useState("s");
+  const [isLogged, setLogged] = useState(false);
+  
   return (
     <div className="App">
-      
+      <header className='blog-header'>
+        <h1> Your Blogger </h1>
+
+        {(username!=="")?<p> Welcome, {username}</p>:null}
+      </header>
     </div>
   );
 }
