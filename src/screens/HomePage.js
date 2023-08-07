b7e55a5b7f72a8684698875c84e685e3f5c3dc76
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-08-07 11:52:49 +0530

    Add feed filter buttons to home page.

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -2,14 +2,19 @@ import SearchBar from "../components/Search";
 import { useState } from "react";
 import '../css/Home.css'
 import FeedButtons from "../components/FeedButtons";
+import HomeFeed from "../components/HomeFeed";
 
 const HomePage = () => {
     const [searchText, setSearchText] = useState("")
     const btnList = [{name:'a', handler:() => {console.log("btn clicked")}},{name:'a', handler:() => {console.log("btn clicked")}},{name:'a', handler:() => {console.log("btn clicked")}}]
+    
+    const feed = [1,2,3];
     return (
         <>
             <SearchBar searchText={searchText} setSearchText={setSearchText}/>
             <FeedButtons btnList={btnList}/>
+
+            <HomeFeed feed={feed}/>
         </>
     )
 }
